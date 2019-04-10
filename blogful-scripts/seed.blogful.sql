c08d1a51f462f120bbe64a0cf55baed3c24e7740
Scott Williams <scottcolemanwilliams@gmail.com>
2019-04-10 15:08:33 -0500

    finished building services practice

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Article1', now() - '21 days'::INTERVAL, 'blah blah one'),
+    ('Article2', now() - '21 days'::INTERVAL, 'blah blah two'),
+    ('Article3', now() - '21 days'::INTERVAL, 'blah blah three'),
+    ('Article4', now() - '21 days'::INTERVAL, 'blah blah four'),
+    ('Article5', now() - '21 days'::INTERVAL, 'blah blah five'),
+    ('Article6', now() - '9 days'::INTERVAL, 'blah blah six'),
+    ('Article7', now() - '9 days'::INTERVAL, 'blah blah seven'),
+    ('Article8', now() - '9 days'::INTERVAL, 'blah blah eight'),
+    ('Article9', now() - '9 days'::INTERVAL, 'blah blah nine'),
+    ('Article10', now() - '9 days'::INTERVAL, 'blah blah ten'),
+    ('Article11', now() - '7 days'::INTERVAL, 'blah blah eleven'),
+    ('Article12', now() - '7 days'::INTERVAL, 'blah blah twelve'),
+    ('Article13', now() - '5 days'::INTERVAL, 'blah blah thirteen'),
+    ('Article14', now() - '5 days'::INTERVAL, 'blah blah fourteen'),
+    ('Article15', now() - '5 days'::INTERVAL, 'blah blah fifteen'),
+    ('Article16', now() - '5 days'::INTERVAL, 'blah blah sixteen'),
+    ('Article17', now() - '4 days'::INTERVAL, 'blah blah seventeen'),
+    ('Article18', now() - '4 days'::INTERVAL, 'blah blah eighteen'),
+    ('Article19', now() - '4 days'::INTERVAL, 'blah blah nineteen'),
+    ('Article20', now() - '3 days'::INTERVAL, 'blah blah twenty')
+;
